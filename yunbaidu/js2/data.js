361567499c30c085fb77ae2d338e9078201523fd
Sroyis <1637933916@qq.com>
2016-07-04 19:34:58 +0800

    data

--- yunbaidu/js2/data.js
+++ yunbaidu/js2/data.js
@@ -1,45 +1,25 @@
-//数据
-var datas = {
-	contextmenu: {
-		//common菜单组
-		common: [{
-			name: "新建文件夹",
-			exe: function() {
-				newFolder();
-				hideonContextmenu();
-			}
-		}, {
-			name: "刷新",
-			exe: function() {
-				Refresh();
-				hideonContextmenu();
-			}
-		}],
-		//文件夹右键菜单
-		filetextmenu:[{
-			name: "重命名",
-			exe: function() {
-				console.log('重命名');
-			}
-		},{
-			name: "新建文件夹",
-			exe: function() {
-				newFolder();
-				hideonContextmenu();
-			}
-		},{
-			
-		}]
-	},
-	file: [{
-		name: 'a1',
-		id: 1,
-		prd: 0,
-		Attri: 'file',
-	}, {
-		name: 'b2',
-		id: 2,
-		prd: 0,
-		Attri: 'file',
-	}]
+var data = {
+	files:[
+		{
+			id:1,
+			pid:0,
+			"name":"我的文档",
+			type:"file"
+		},
+		{
+			id:2,
+			pid:0,
+			"name":"我的音乐"
+		},
+		{
+			id:3,
+			pid:1,
+			"name":"我的音乐"
+		},
+		{
+			id:4,
+			pid:3,
+			"name":"abc"
+		}
+	]
 }
